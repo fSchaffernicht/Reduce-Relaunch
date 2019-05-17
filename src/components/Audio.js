4d658af89a16c9e6d392456a2dc97eee013c0da7
Felix Schaffernicht <felix@schaffernicht.com>
2019-05-17 19:11:10 +0200

    svg elements to audio player

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -107,7 +107,7 @@ const Pause = () => (
     <g
       id='Page-1'
       stroke='none'
-      stroke-width='1'
+      strokeWidth='1'
       fill='none'
       fill-rule='evenodd'
     >
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function Contact() {
-  return <div>contact</div>
+  return (
+    <div>
+      <h1>E-Mail... immer noch bewährt</h1>
+      <p>reduce.mail@google oder so</p>
+    </div>
+  )
 }
